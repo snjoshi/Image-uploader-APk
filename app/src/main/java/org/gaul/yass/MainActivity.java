878f17abf0d94a221f5329a9c9f5989a54672a67
Sumedh Joshi <joshisumedh13@gmail.com>
2020-11-02 12:54:59 +0530

    Update MainActivity.java

--- app/src/main/java/org/gaul/yass/MainActivity.java
+++ app/src/main/java/org/gaul/yass/MainActivity.java
@@ -811,10 +811,10 @@ public final class MainActivity extends AppCompatActivity {
         YassPreferences(Context context) {
             SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
             // TODO: should default values be null?
-            this.accessKey = "IQLTGIEXYLPOP597U7H0";//prefs.getString("access_key", "access_key");
-            this.secretKey = "O57ghawVXgUAdYaLsOe0Pgv4UwM6A7hWFtmk1SLh";//prefs.getString("secret_key", "secret_key");
-            this.bucketName = "designers";//prefs.getString("bucket_name", "bucket_name");
-            this.endpoint = "https://s3.wasabisys.com";//prefs.getString("endpoint", null);
+            this.accessKey = "";//prefs.getString("access_key", "access_key");
+            this.secretKey = "";//prefs.getString("secret_key", "secret_key");
+            this.bucketName = "";//prefs.getString("bucket_name", "bucket_name");
+            this.endpoint = "";//prefs.getString("endpoint", null);
             this.cameraUpload = prefs.getBoolean("camera_upload", false);
             this.cameraUploadOnlyOnWifi = prefs.getBoolean("camera_upload_only_on_wifi", false);
         }
